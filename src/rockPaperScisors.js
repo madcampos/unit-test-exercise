9ea43043e33aebaae8b1ce8970b0deecc60a775c
madcampos <madcampos@outlook.com>
2020-09-28 00:18:15 -0400

    Adiciona arquivos do desafio

---
+++ src/rockPaperScisors.js
@@ -0,0 +1,9 @@
+/**
+ * @file Função de `Pedra, Papel e Tesoura`.
+ * @author Seu nome aqui
+ * @version 1.0.0
+ */
+
+module.exports = function rockPaperScisors(player1, player2) {
+	// TODO: implementar
+};
---
+++ test/rockPaperScisors.test.js
@@ -0,0 +1,6 @@
+/**
+ * @file Teste da função `Pedra, Papel e Tesoura`.
+ * @author Seu nome aqui
+ * @version 1.0.0
+ */
+const tap = require('tap');
